f676a9e8cc14564fe8cf9a3b47c628e5d8d9e03a
Maziar <maziarmoslemi@gmail.com>
2025-01-20 12:16:29 +0330

    Redirect to specific page on enter except redeem page

--- middleware/redirect-to-gift.global.ts
+++ middleware/redirect-to-gift.global.ts
@@ -1,7 +1,8 @@
 // This middleware should be ommited in future
-// There is no other page except gift-code implemented yet 
-
-export default defineNuxtRouteMiddleware((from, to)=>{
-    if(to.path !== "/redeem-gift-card" && from.path !== "/redeem-gift-card") 
-        return navigateTo("/redeem-gift-card")
-})
+// There is no other page except gift-code implemented yet
+const validPath = ["/redeem-gift-card", "/scan-gift"];
+export default defineNuxtRouteMiddleware((from, to) => {
+  if (!validPath.includes(to.path) && !validPath.includes(from.path)) {
+    return navigateTo("/scan-gift");
+  }
+});
