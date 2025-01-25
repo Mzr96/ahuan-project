2d62ac607ec299e15620d3af1e2fb531312e3cd5
Maziar <maziarmoslemi@gmail.com>
2025-01-25 17:22:46 +0330

    Increase border radius for button and text input

--- plugins/vuetify/defaults.ts
+++ plugins/vuetify/defaults.ts
@@ -2,10 +2,13 @@ export default {
   VBtn: {
     size: "large",
     color: "primary",
+    rounded: "lg",
   },
   VTextField: {
     color: "primary",
     variant: "outlined",
+    rounded: "lg",
+    density: "comfortable",
   },
   VOtpInput: {
     variant: "outlined",
