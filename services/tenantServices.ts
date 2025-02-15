7b912a39eef490fd5f8e79c77e39459e4c65d5c0
Maziar <maziarmoslemi@gmail.com>
2025-02-15 15:19:44 +0330

    Change end-point

--- services/tenantServices.ts
+++ services/tenantServices.ts
@@ -1,12 +1,9 @@
+import type { DefaultResponse } from "~/types/ApiResponse";
 import type { Tenant } from "~/utils/validation/tenantSchema";
 
 export const getTenant = async (dsCode: string) => {
-  const response: Tenant = await $api(
-    // FIXME :: change to actual end-point
-    `http://localhost:5000/tenant/${dsCode}`,
-    {
-      method: "GET",
-    }
-  );
-  return response;
+  const response: DefaultResponse<Tenant> = await $api(`api/tenant/${dsCode}`, {
+    method: "GET",
+  });
+  return response.data;
 };
