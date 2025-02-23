823fdd2edfaa02ad6bdd155854476204cbc83e86
Maziar <maziarmoslemi@gmail.com>
2025-02-23 17:13:57 +0330

    Load logo from tenant

--- views/redeem/RedeemCongrats.vue
+++ views/redeem/RedeemCongrats.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-primary-lighten-1 d-flex flex-column py-1">
-    <VImg width="120" class="mx-auto" src="/public/images/logo.png" />
+    <VImg width="120" class="mx-auto" :src="tenant?.logo" />
     <VImg width="250" class="mx-auto pt-10" src="/public/images/gift.png" />
     <p class="text-center text-primary font-weight-bold">
       کارت هدیه {{ tenant?.name }}
