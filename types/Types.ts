26b0aac6d75166f2abf0eb3f11a8f86a2143f68e
Maziar <maziarmoslemi@gmail.com>
2025-03-15 09:56:51 +0330

    Add InstrumentDetailModal component and update Types for modal content

--- types/Types.ts
+++ types/Types.ts
@@ -2,3 +2,10 @@ export interface InstrumentPortion {
   id: string;
   percentage: number;
 }
+
+export interface InstrumentDetailModalContent {
+  title: string;
+  descriptin?: string;
+  icon: string;
+  color: string;
+}
