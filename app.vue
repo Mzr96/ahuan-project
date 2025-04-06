9470ce75fe5cdc0b99740b20a60a7c3c29a2d7f3
Maziar <maziarmoslemi@gmail.com>
2025-04-06 14:56:46 +0330

    Move snackbar to app.vue

--- app.vue
+++ app.vue
@@ -2,19 +2,22 @@
   <VApp>
     <NuxtLayout>
       <NuxtPage />
+      <RabinSnackbar v-model="isVisible" :type="selectedType" :message />
     </NuxtLayout>
   </VApp>
 </template>
 <script setup lang="ts">
 import { useTheme } from "vuetify";
+import RabinSnackbar from "~/components/RabinSnackbar.vue";
+import { useSnackbar } from "~/composables/useSnackbar";
+const { message, selectedType, isVisible } = useSnackbar();
 
 useHead({ title: "کارت هدیه" });
 
-const router = useRouter();
 const theme = useTheme();
 const tenantStore = useTenantStore();
 
-if (!tenantStore.tenant) router.push("/down-time");
+if (!tenantStore.tenant) navigateTo("/down-time");
 
 theme.themes.value.light.colors = {
   ...theme.themes.value.light.colors,
